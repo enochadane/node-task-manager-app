2d612c8afe09043a56ca7bf022a1d5a8d11beefa
enochadane <henokadane8@gmail.com>
2022-03-13 01:03:16 +0300

    add a welcome route

--- src/index.js
+++ src/index.js
@@ -12,6 +12,10 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+app.get('/', (req, res) => {
+    res.status(200).send({ success: true, message: 'Welcom to Task App!' })
+})
+
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
 })
