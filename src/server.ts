96c33df47325891194da35f82f0733d8cbc47d1f
enochadane <henokadane8@gmail.com>
2022-03-25 02:12:44 +0300

    update npm packages

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
 import app from "./app";
 import configs from "./config/config";
 
-import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "../swagger.json";
 const port = configs.PORT;
 
 app.listen(port, () => {
