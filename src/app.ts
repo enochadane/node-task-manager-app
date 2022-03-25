4b06f44db311224417cb436a5fd753c51e1398c5
enochadane <henokadane8@gmail.com>
2022-03-25 13:09:42 +0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../swagger.json";
@@ -7,6 +8,8 @@ import swaggerDocument from "../swagger.json";
 import { authRouter, userRouter, taskRouter } from "./routes/v1/index";
 
 const app = express();
+
+app.use(cors())
 
 app.use(express.json({ limit: "2mb" }));
 
